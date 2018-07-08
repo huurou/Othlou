c91532f09711e00de40ee8029742c7c75fd0bd4a
ふうろう <114514+huurou@users.noreply.github.com>
2018-07-08 19:28:18 +0900

    u

--- othero_modelkifu.py
+++ othero_modelkifu.py
@@ -3,8 +3,10 @@ import random
 from datetime import datetime
 import numpy as np
 from tqdm import tqdm
-from keras.models import load_model
+from keras.layers import Activation, Dense, Dropout
+from keras.models import Sequential, load_model
 KIFU_NUM = 5000
+model = load_model('othlo.h5')
 BLACK = np.array([[0, 0, 0, 0, 0, 0, 0, 0  # 黒石初期配置
                  , 0, 0, 0, 0, 0, 0, 0, 0
                  , 0, 0, 0, 0, 0, 0, 0, 0
@@ -66,21 +68,39 @@ class Play:  # 石,盤面の処理について
                     legal = np.append(legal, np.array([[i, j]]), axis=0)
         return legal
 
-        def set_trun(self, board, color, y, x):  # 石を打ち、ひっくり返す(board情報が書き換えられる)
-            cnt = 0
-            for d in range(-1, 2):
-                for e in range(-1, 2):
-                    if d == 0 and e == 0:
+    def exist_legal_move(self, board, color):
+        for i in range(1, 9):
+            for j in range(1, 9):
+                if self.is_legal(board, color, i, j):
+                    return 1
+        return 0
+
+    def set_trun(self, board, color, y, x):  # 石を打ち、ひっくり返す(board情報が書き換えられる)
+        cnt = 0
+        for d in range(-1, 2):
+            for e in range(-1, 2):
+                if d == 0 and e == 0:
                         continue
-                    cnt = self.count_trun_over(board, color, y, x, d, e)
-                    for i in range(1, cnt + 1):
+                cnt = self.count_trun_over(board, color, y, x, d, e)
+                for i in range(1, cnt + 1):
                         board[y + i * d][x + i * e] = color
-            board[y][x] = color
+        board[y][x] = color
+    
+    def ban_convert(self, board):
+        reban = ban[1:9, 1:9]
+        b = np.zeros((8, 8))
+        w = np.zeros((8, 8))
+        b[reban == 1] = 1
+        w[reban == 2] = 1
+        black = np.reshape(b, (1, 64))
+        white = np.reshape(w, (1, 64))
+        return np.c_[black, white]
 
 
 class Model:  # モデル関係
-    def predict_result(self, board, color, y, x):
-        model = load_model('othlo.h5')
+    def predict(self, ban):  # 予測値を返す
+        pred = model.predict(ban, batch_size=1)
+        return pred
 
 
 class Move:  # 打つ手を決める
@@ -93,10 +113,8 @@ class Move:  # 打つ手を決める
         return y, x
     
     def decide_move(self, board, color):
-        rate = []
-        l = Play().legal_list(board, color)
+        tsugiban = copy.deepcopy(board)
+        l = Play().legal_list(tsugiban, color)
         if l.shape[0] == 1:
             y, x = l[0]
-        for i in range(l.shape[0]):
-            q, p = l[i]
-            rate.appned()
+        
